5ec7208f249aae4b3edbce1be59f5fb379026917
AaminaBinteArif <aaminabintearif@gmail.com>
2025-07-12 19:06:50 +0500

    Removed hardcoded IP address and made it dynamic

--- BAsic packet sniffer.py
+++ BAsic packet sniffer.py
@@ -1,7 +1,8 @@
 import socket
 import struct
 sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-sniffer.bind(("192.168.1.5", 0))
+host = socket.gethostbyname(socket.gethostname())
+sniffer.bind((host, 0))
 sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 try:
